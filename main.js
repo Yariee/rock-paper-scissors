b3bd024e0730c7bd2fd23f8da6dba6186b096452
Yair <ballinasyair@gmail.com>
2023-03-16 19:11:00 -0500

    implemented getComputerChoice function

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 console.log("Hello, World!"); /* test to make sure I linked javascript to HTML file correctly! */ 
+
+
+/* Function to get a random string */
+function getComputerChoice() {
+    let randomString = ["Rock", "Paper", "Scissors"];
+    let returnString = randomString[Math.floor(Math.random() * randomString.length)];
+    return returnString;
+}
+
+/* Testing to make sure we get random string of rock, paper, or scissors */
+console.log(getComputerChoice()); 
