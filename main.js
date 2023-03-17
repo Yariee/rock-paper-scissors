85e89111e4078dab237816d2ab0bafe78a31e9b2
Yair <ballinasyair@gmail.com>
2023-03-16 23:06:56 -0500

    finished the first part of game

--- main.js
+++ main.js
@@ -1,41 +1,59 @@
 // Function to get a random string 
 function getComputerChoice() {
-    let randomString = ["Rock", "Paper", "Scissors"];
+    let randomString = ["rock", "paper", "scissors"];
     let returnString = randomString[Math.floor(Math.random() * randomString.length)];
     return returnString;
 }
 
 function playRound(playerSelection, computerSelection) {
-    // Code goes here
-    if (playerSelection == "Rock") {
-        if (computerSelection == "Rock") {
-            return "The game is a tie!";
-        } else if (computerSelection == "Paper") {
-            return "Sorry, you have lost!";
+    if (playerSelection == "rock") {
+        if (computerSelection == "rock") {
+            return "Tie";
+        } else if (computerSelection == "paper") {
+            return "Lost";
         } else {    // Scissors outcome
-            return "You have won the game!";
+            return "Win";
         }
-    } else if (playerSelection == "Paper") {
-        if (computerSelection == "Rock") {
-            return "You have won the game!";
-        } else if (computerSelection == "Paper") {
-            return "The game is a tie!";
+    } else if (playerSelection == "paper") {
+        if (computerSelection == "rock") {
+            return "Win";
+        } else if (computerSelection == "paper") {
+            return "Tie";
         } else {    // Scissors outcome
-            return "Sorry, you have lost the game!";
+            return "Lost";
         }
     } else {    // If playerSelection is scissors
-        if (computerSelection == "Rock") {
-            return "Sorry, you have lost the game!";
-        } else if (computerSelection == "Paper") {
-            return "You have won the game!";
+        if (computerSelection == "rock") {
+            return "Lost";
+        } else if (computerSelection == "paper") {
+            return "Win";
         } else {
-            return "The game is tied!";
+            return "Tie";
         }
     }
 }
 
+function game() {
+    let win = 0;
+    let lost = 0;
+    let tie = 0;
 
-// Testing to make sure we're playing the game correctly with hardcode input of playerSelection
-const playerSelection = "Rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+    for (let i = 0; i < 5; i++) {
+        let choice = prompt("Would you like to be Rock, Paper, or Scissors?");
+        // Helps lowercase all lets in case of caps error
+        let playerSelection = choice.toLowerCase();
+
+        if (playRound(playerSelection, getComputerChoice()) == "Win") {
+            win += 1;
+        } else if (playRound(playerSelection, getComputerChoice()) == "Lost") {
+            lost += 1;
+        } else {    // Tie Scenario
+            tie += 1;
+        }
+    }
+    console.log("Here are your wins: " + win);
+    console.log("Here are your losses: " + lost);
+    console.log("Here are your ties: " + tie);
+}
+
+console.log(game());
